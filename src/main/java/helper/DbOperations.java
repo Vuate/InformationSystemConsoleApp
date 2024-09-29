f1dc8d70ec9bfd9f0194d70d37004f8bbaecb59c
ardae <ardaemreci2@gmail.com>
2024-09-29 19:22:31 +0300

    delete method added

--- src/main/java/helper/DbOperations.java
+++ src/main/java/helper/DbOperations.java
@@ -14,6 +14,7 @@ public class DbOperations {
         ps.setString(2, surname);
         ps.setString(3, mail);
         ps.setString(4, phone);
+
         ps.executeUpdate();
     }
 
@@ -27,4 +28,14 @@ public class DbOperations {
             System.out.println("*ID: " + rs.getInt("id") + " *NAME: " + rs.getString("name") + " *SURNAME: " + rs.getString("surname") + " *MAIL: " + rs.getString("mail") + " *NUMBER: " + rs.getString("phone_number"));
         }
     }
+
+    public void deleteInfo(String id) throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/information_system?user=root&&password=1234");
+        String query = "DELETE FROM user WHERE (id) = (?)";
+        PreparedStatement ps = connection.prepareStatement(query);
+        ps.setString(1, id);
+        ps.executeUpdate();
+    }
+
+
 }
