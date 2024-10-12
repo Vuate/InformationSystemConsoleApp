1412dc3d3c4f1c26675574e1d73150283bc2f0af
ardae <ardaemreci2@gmail.com>
2024-10-12 15:00:19 +0300

    reformat code

--- src/main/java/helper/DbOperations.java
+++ src/main/java/helper/DbOperations.java
@@ -5,22 +5,18 @@ import java.sql.*;
 public class DbOperations {
 
     public void insertInfo(String name, String surname, String mail, String phone) throws SQLException {
-
         Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/information_system?user=root&&password=1234");
         String query = "INSERT INTO user (name, surname, mail, phone_number) VALUES (?, ?, ?, ?)";
         PreparedStatement ps = connection.prepareStatement(query);
-
         ps.setString(1, name);
         ps.setString(2, surname);
         ps.setString(3, mail);
         ps.setString(4, phone);
-
         ps.executeUpdate();
         System.out.println("Kullanıcı başarıyla eklendi !");
     }
 
     public static void selectInfo() throws SQLException {
-
         Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/information_system?user=root&&password=1234");
         Statement st = connection.createStatement();
         ResultSet rs = st.executeQuery("SELECT * FROM user");
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,14 +10,12 @@ public class Main {
 
     public static void main(String[] args) throws SQLException {
         start();
-
     }
 
     public static void start() throws SQLException {
         System.out.println("Bilgi Sistemine Hoşgeldiniz!");
         System.out.println("Lütfen yapmak istediğiniz işlemi seçiniz:");
         System.out.println("1- kayıt göster - 2- kayıt ekle - 3- kayıt sil");
-
 
         int choose;
         Scanner scan = new Scanner(System.in);
@@ -27,19 +25,18 @@ public class Main {
             dbOperations.selectInfo();
         } else if (choose == 2) {
             scan = new Scanner(System.in);
+
             System.out.println("İsim giriniz :");
-
             String name = scan.nextLine();
+
             System.out.println("Soyad giriniz :");
-
             String surname = scan.nextLine();
+
             System.out.println("Mail giriniz :");
-
             String mail = scan.nextLine();
+
             System.out.println("Telefon numarası giriniz :");
-
             String phone = scan.nextLine();
-
 
             dbOperations.insertInfo(name, surname, mail, phone);
         } else if (choose == 3) {
@@ -47,12 +44,9 @@ public class Main {
             System.out.println("Silmek istediğiniz ıd :");
             String id = scan.nextLine();
             dbOperations.deleteInfo(id);
-
         } else {
             System.out.println("Yanlış seçim yaptınız");
         }
         start();
     }
-
-
 }
